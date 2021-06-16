d92b9a3295d6ff1cd3eda67cae94b20526462b27
kasaikoun <kasiakou.n@gmail.com>
2021-06-16 21:42:41 +0300

    Fix syntax errors in classes which have already created on basis of tslint config

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { Employee } from "./test_export/employee";
-import { myContainer } from "./inversify.config";
-import { IService } from "./services/i_service";
+import { Employee } from './test_export/employee';
+import { myContainer } from './inversify.config';
+import { IService } from './services/i_service';
 
 const service = myContainer.get<IService>(nameof<IService>());
-var className = nameof<Employee>();
-var employee = new Employee("nikita", "kasiakou");
+const className = nameof<Employee>();
+const employee = new Employee('nikita', 'kasiakou');
 document.body.innerHTML = `service.getData: ${service.getData()}`;
-console.log("test test");
+console.log('test test');

c6e624d39b9126788afd371926789c0dee14d6d8
kasaikoun <kasiakou.n@gmail.com>
2021-07-03 17:49:08 +0300

    Replace interface Entity with class to add implementation for members Add Unit class

--- src/entities/entity.ts
+++ src/entities/entity.ts
@@ -1,7 +1,15 @@
+import { SpriteSheet } from '../animations/sprite_sheet';
 import { Observable } from '../observable';
+import { Point } from '../point';
 import { Transform } from '../transform';
 
-export interface Entity {
-  transform: Transform;
-  updated: Observable;
+export class Entity {
+  readonly transform: Transform;
+  readonly updated: Observable = new Observable();
+
+  // todo: TEMP consturctor
+  constructor (readonly spriteSheet: SpriteSheet) {
+    this.transform = new Transform();
+    this.transform.cartesianPosition = new Point(100, 100);
+  }
 }

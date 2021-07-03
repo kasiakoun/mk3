640d8c11b9a86eae8490c6d40c2d560272db7253
kasaikoun <kasiakou.n@gmail.com>
2021-07-03 17:54:14 +0300

    Add start function to start game from this initial point

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,3 @@
-import { Employee } from './test_export/employee';
-import { myContainer } from './inversify.config';
-import { IService } from './services/i_service';
+import { start } from './ui/start';
 
-const service = myContainer.get<IService>(nameof<IService>());
-const className = nameof<Employee>();
-const employee = new Employee('nikita', 'kasiakou');
-document.body.innerHTML = `service.getData: ${service.getData()}`;
-console.log('test test');
+start();
---
+++ src/ui/start.ts
@@ -0,0 +1,38 @@
+import { createSpriteSheet } from '../animations/sprite_sheet_factory';
+import { CoordinateConverter } from '../converters/coordinate_converter';
+import { Entity } from '../entities/entity';
+import { UnitName } from '../entities/unit_name';
+import { container } from '../inversify.config';
+import { ParabolaMotion } from '../motions/parabola_motion';
+
+export function start() {
+  const gameElement = document.getElementById('game');
+  // todo: TEMP init constructor by converter
+  const spriteSheet = createSpriteSheet(UnitName.Cyrax);
+  const entity = new Entity(spriteSheet);
+  entity.updated.subscribe(() => {
+    const unitElement = createUnitElement(entity);
+    if (gameElement !== null) {
+      gameElement.innerHTML = unitElement.outerHTML;
+      // gameElement.appendChild(unitElement);
+    }
+  });
+  const coordinateConverter = container.get<CoordinateConverter>(nameof<CoordinateConverter>());
+  const motion = new ParabolaMotion(entity, coordinateConverter);
+  motion.start();
+}
+
+function createUnitElement(entity: Entity): Element {
+  const unitElement = document.createElement('div');
+
+  unitElement.style.background = `url(${entity.spriteSheet.image}) no-repeat`;
+  unitElement.style.position = 'absolute';
+  unitElement.style.width = `${entity.spriteSheet.currentFrame.width}px`;
+  unitElement.style.height = `${entity.spriteSheet.currentFrame.height}px`;
+  unitElement.style.backgroundPosition = `-${entity.spriteSheet.currentFrame.offset.x}px -${entity.spriteSheet.currentFrame.offset.y}px`;
+
+  unitElement.style.marginLeft = `${entity.transform.position.x}px`;
+  unitElement.style.marginTop = `${entity.transform.position.y}px`;
+
+  return unitElement;
+}

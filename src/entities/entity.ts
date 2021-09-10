f0f083e65440b7eec39a376cca2e63e35f0169a8
kasaikoun <kasiakou.n@gmail.com>
2021-09-11 01:42:23 +0300

    Add getters for transform and updated fields

--- src/entities/entity.ts
+++ src/entities/entity.ts
@@ -4,12 +4,20 @@ import { Point } from '../point';
 import { Transform } from '../transform';
 
 export class Entity {
-  readonly transform: Transform;
-  readonly updated: Observable = new Observable();
+  readonly #transform: Transform;
+  readonly #updated: Observable = new Observable();
+
+  get transform(): Transform {
+    return this.#transform;
+  }
+
+  get updated(): Observable {
+    return this.#updated;
+  }
 
   // todo: TEMP consturctor
   constructor (readonly spriteSheet: SpriteSheet) {
-    this.transform = new Transform();
-    this.transform.cartesianPosition = new Point(400, 50);
+    this.#transform = new Transform();
+    this.#transform.cartesianPosition = new Point(400, 50);
   }
 }

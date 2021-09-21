1810c1bd88712187eef735c73ae11697fe04fc50
kasaikoun <kasiakou.n@gmail.com>
2021-09-21 16:44:37 +0300

    Modify Observable so that generic type is added

--- src/observable.ts
+++ src/observable.ts
@@ -1,17 +1,17 @@
-export class Observable {
-  private observers: (() => void)[] = [];
+export class Observable<T = void> {
+  private observers: ((param: T) => void)[] = [];
 
-  subscribe(action: () => void) {
+  subscribe(action: (param: T) => void) {
     this.observers.push(action);
   }
 
-  unsubscribe(action: () => void) {
+  unsubscribe(action: (param: T) => void) {
     const foundIndex = this.observers.indexOf(action);
     this.observers.splice(foundIndex, 1);
   }
 
-  fire() {
-    this.observers.forEach(p => p());
+  fire(param: T) {
+    this.observers.forEach(p => p(param));
   }
 
   clear() {

b16a6176e2abf1f9171b44131f3fc8e61275059f
kasaikoun <kasiakou.n@gmail.com>
2021-09-23 16:34:37 +0300

    Add check on signatures which has been added Replace indexOf with findIndex because arrow-functions can have diffrenet references

--- src/observable.ts
+++ src/observable.ts
@@ -2,11 +2,16 @@ export class Observable<T = void> {
   private observers: ((param: T) => void)[] = [];
 
   subscribe(action: (param: T) => void) {
+    const signatureAlreadyAdded = this.observers.some(p => p.toString() === action.toString());
+    if (signatureAlreadyAdded) {
+      throw new Error(`Function has been already "${action.toString()}" subscribed`);
+    }
     this.observers.push(action);
   }
 
   unsubscribe(action: (param: T) => void) {
-    const foundIndex = this.observers.indexOf(action);
+    const foundIndex = this.observers.findIndex(p => p.toString() === action.toString());
+    if (foundIndex < 0) return;
     this.observers.splice(foundIndex, 1);
   }
 

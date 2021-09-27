d4362ba1b9587d23324bd949689ca28f3c303d1f
kasaikoun <kasiakou.n@gmail.com>
2021-09-28 00:33:22 +0300

    Add generic parameter type for Observable class

--- src/observable.ts
+++ src/observable.ts
@@ -1,7 +1,7 @@
-export class Observable<T = void> {
-  private observers: ((param: T) => void)[] = [];
+export class Observable<T1 = void, T2 = void> {
+  private observers: ((param1: T1, param2: T2) => void)[] = [];
 
-  subscribe(action: (param: T) => void) {
+  subscribe(action: (param1: T1, param2: T2) => void) {
     const signatureAlreadyAdded = this.observers.some(p => p.toString() === action.toString());
     if (signatureAlreadyAdded) {
       throw new Error(`Function has been already "${action.toString()}" subscribed`);
@@ -9,14 +9,14 @@ export class Observable<T = void> {
     this.observers.push(action);
   }
 
-  unsubscribe(action: (param: T) => void) {
+  unsubscribe(action: (param1: T1, param2: T2) => void) {
     const foundIndex = this.observers.findIndex(p => p.toString() === action.toString());
     if (foundIndex < 0) return;
     this.observers.splice(foundIndex, 1);
   }
 
-  fire(param: T) {
-    this.observers.forEach(p => p(param));
+  fire(param1: T1, param2: T2) {
+    this.observers.forEach(p => p(param1, param2));
   }
 
   clear() {

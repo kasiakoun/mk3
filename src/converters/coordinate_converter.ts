ce9e53479014df854a75edf03e01f9786cd24ab1
kasaikoun <kasiakou.n@gmail.com>
2021-10-10 01:20:27 +0300

    Add factory method for creating coordinateConverter to pass dynamic parameters

--- src/converters/coordinate_converter.ts
+++ src/converters/coordinate_converter.ts
@@ -1,16 +1,14 @@
 import { inject, injectable } from 'inversify';
-import { Game } from '../game';
+import { ArenaView } from '../arenas/common/arena_view';
 import { Point } from '../point';
 
 @injectable()
 export class CoordinateConverter {
-  constructor (
-    @inject(nameof<Game>()) private readonly game: Game) {
-  }
+  constructor (private readonly arenaView: ArenaView) {}
 
   convertCartesianToScreen(caresianPoint: Point): Point {
     const screenX = caresianPoint.x;
-    const screenY = -caresianPoint.y + this.game.arenaHeight;
+    const screenY = -caresianPoint.y + this.arenaView.height;
 
     return new Point(screenX, screenY);
   }

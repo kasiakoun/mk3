dea566bf456d2d5ca622ad2663f23d00e72578b8
kasaikoun <kasiakou.n@gmail.com>
2021-11-25 00:45:39 +0300

    Add BackwardWalkState state Rename WalkState as ForwardWalkState Add BackWalkMotion to give to Unit the possibility of walking backward

--- src/entities/states/forward_walk_state.ts
+++ src/entities/states/forward_walk_state.ts
@@ -1,10 +1,8 @@
 import { ForwardWalkMotion } from '../../motions/forward_walk_motion';
 import { StateBase } from './state_base';
 
-export class WalkState extends StateBase {
+export class ForwardWalkState extends StateBase {
   async promote() {
-    // todo: depend on unit direction
-    // todo: replace motion with action in StateBase
     this.motion = new ForwardWalkMotion(this.unit);
     await this.motion.start();
   }
